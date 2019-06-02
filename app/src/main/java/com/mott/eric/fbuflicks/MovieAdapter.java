4ffcea4a1849fa63395bac5e9ae89d6beac07c6e
emott10 <ericmott10@gmail.com>
2019-06-02 14:48:26 -0700

    Initial commit

---
+++ app/src/main/java/com/mott/eric/fbuflicks/MovieAdapter.java
@@ -0,0 +1,103 @@
+package com.mott.eric.fbuflicks;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.mott.eric.fbuflicks.models.Config;
+import com.mott.eric.fbuflicks.models.Movie;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    //list of movies
+    ArrayList<Movie> movies;
+    Context context;
+    Config config;
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+//initialize with list
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    //@NonNull
+    //creates and inflates a new view
+    @Override
+    public ViewHolder onCreateViewHolder(/*@NonNull*/ ViewGroup parent, int viewType) {
+        //create the inflater
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //create the view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    //binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(/*@NonNull*/ ViewHolder holder, int position) {
+        //get the movie data at the specified position
+        Movie movie = movies.get(position);
+        //populate the view with the movie data
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        //determine orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        String imageUrl;
+
+        //if in portrait, load poster image
+        if(isPortrait){
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageUrl =config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage : holder.ivBackdropImage;
+
+        Glide.with(context)
+                .load(imageUrl)
+                .bitmapTransform(new RoundedCornersTransformation(context, 15, 0))
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
+    }
+
+    //returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    //create the viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        //track view objects
+        ImageView ivPosterImage;
+        ImageView ivBackdropImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPosterImage = itemView.findViewById(R.id.ivPoster);
+            ivBackdropImage = itemView.findViewById(R.id.ivBackdropImage);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+        }
+    }
+}
